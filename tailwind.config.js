7fa798d10ecbdfd48805a0fc51a85c019c961fe0
Mustafo-Odilzoda <144124566+Mustafo-Odilzoda@users.noreply.github.com>
2023-10-10 19:03:23 +0500

    добавил мясо
    сделал что то похожее на проект

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            myyrcozelen: '#8DF58C',
+            mytemnozelen: '#9AD479',
+            myheitralnu: '#D1EB91',
+            mytemnogoltui: '#D4D279',
+            myyrcogoltui: '#F5E98C',
+
+        }
+      },
     },
     plugins: [],
   }
