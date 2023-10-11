dd2ce664236532da25db82832b56c4e44a8be39d
Mustafo-Odilzoda <144124566+Mustafo-Odilzoda@users.noreply.github.com>
2023-10-11 19:01:30 +0500

    анимация обложки
    сделал живое меню добавил шрифт надо доработать его ещё доработать

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
-        colors: {
-            myyrcozelen: '#8DF58C',
-            mytemnozelen: '#9AD479',
-            myheitralnu: '#D1EB91',
-            mytemnogoltui: '#D4D279',
-            myyrcogoltui: '#F5E98C',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        myyrcozelen: '#8DF58C',
+        mytemnozelen: '#9AD479',
+        myheitralnu: '#D1EB91',
+        mytemnogoltui: '#D4D279',
+        myyrcogoltui: '#F5E98C',
 
-        }
+      },
+      fontFamily: {
+      sans: ['Pixelify Sans'], 
       },
     },
-    plugins: [],
+  },
+plugins: [],
   }
